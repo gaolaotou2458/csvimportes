93f87232cebb5ad25d92397ce7688955116dab65
xuxk <xkxu@zhibodata.cn>
2020-08-17 16:38:28 +0800

    init

---
+++ src/main/java/com/imooc/dianping/EsCsvImporterApplication.java
@@ -0,0 +1,13 @@
+package com.imooc.dianping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsCsvImporterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsCsvImporterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/imooc/dianping/EsCsvImporterApplicationTests.java
@@ -0,0 +1,16 @@
+package com.imooc.dianping;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EsCsvImporterApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
